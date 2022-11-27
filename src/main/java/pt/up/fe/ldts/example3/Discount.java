669546d82ec3fbd245e751072a034b2b0477141c
Tomás Palma <up202108880@edu.fe.up.pt>
2022-11-27 08:24:52 +0000

    example 3

---
+++ src/main/java/pt/up/fe/ldts/example3/Discount.java
@@ -0,0 +1,5 @@
+package pt.up.fe.ldts.example3;
+
+public interface Discount {
+    double applyDiscount(double price);
+}
---
+++ src/main/java/pt/up/fe/ldts/example3/SimpleOrder.java
@@ -0,0 +1,18 @@
+package pt.up.fe.ldts.example3;
+
+public class SimpleOrder {
+    private Discount discount;
+    private double price;
+
+    public SimpleOrder(double price) {
+        this.price = price;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public double getTotal() {
+        return this.discount.applyDiscount(price);
+    }
+}
